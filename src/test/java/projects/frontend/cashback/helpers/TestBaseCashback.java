a422f0ae054212405c3ba12cce143e135fc36fdd
rimma2705 <rimma2705@mail.ru>
2020-10-12 13:08:09 +0300

    add test registr

--- src/test/java/projects/frontend/cashback/helpers/TestBaseCashback.java
+++ src/test/java/projects/frontend/cashback/helpers/TestBaseCashback.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBaseCashback {
+    public String paramURL = "";   // "dev"   "stage"  ""
 
     @BeforeMethod(alwaysRun = true)
     public void start(){
